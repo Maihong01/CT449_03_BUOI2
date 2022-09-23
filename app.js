e9185fba9a436066ca5917c43e93025b19051fbf
Maihong01 <hongb1910520@student.ctu.edu.vn>
2022-09-23 15:51:15 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
-// handle 404 response
+
 app.use((req, res, next) => {
     // Code ở đây sẽ chạy khi không có route được định nghĩa nào
     // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
