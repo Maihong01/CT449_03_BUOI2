e853cbc787199460fd18ef2474e2d3889af20336
Maihong01 <hongb1910520@student.ctu.edu.vn>
2022-09-23 14:32:02 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const expree = require("express");
+const contactsRouter = require("./app/routes/contact.route");
+
 const cors = require("cors");
 const express = require("express");
 const req = require("express/lib/request");
@@ -8,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contack book application." });
